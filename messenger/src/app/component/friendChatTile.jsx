b03f87c32ddc4c91c0acafaace823c198852fabd
arjit <apk20023110@gmail.com>
2024-07-08 18:17:03 +0530

    chat1

--- messenger/src/app/component/friendChatTile.jsx
+++ messenger/src/app/component/friendChatTile.jsx
@@ -3,7 +3,7 @@ export default function FriendChatTile() {
     <div className="w-full">
       <div className="max-w-96 w-fit break-words  m-2
     border-solid border-2 border-neutral-900 rounded-r-lg rounded-b-lg px-2 py-1 bg-neutral-900 text-neutral-200">
-        Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum asd asd asd apsnkl whoig uoiieru oiwef fosjddlfk jslkdj flksdj flkj sflk lksdf
+        Lorem ipsum orem ipsum Lorem ipsum Lorem ipsum asd asd asd apsnkl whoig uoiieru oiwef fosjddlfk jslkdj flksdj flkj sflk lksdf
       </div>
     </div>
   )
